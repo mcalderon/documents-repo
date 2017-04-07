cf45081903fb3ebb4395219e46ba0402baaaeab5
Michael Calderon <michael@firstfactory.com>
2017-04-07 10:36:11 -0600

    Added entry points that will be required

--- src/main/java/com/firstfactory/api/resources/DocumentResources.java
+++ src/main/java/com/firstfactory/api/resources/DocumentResources.java
@@ -1,10 +1,18 @@
 package com.firstfactory.api.resources;
 
+import org.glassfish.jersey.media.multipart.FormDataBodyPart;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.InputStream;
 
 @Path("documents")
 public class DocumentResources {
@@ -12,6 +20,28 @@ public class DocumentResources {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAllDocuments() {
-        return Response.ok().entity("{}").build();
+        String response = "List all documents: true";
+        return Response.ok().entity(response).build();
+    }
+
+    @GET
+    @Path("/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getOneDocument(@PathParam("name") String fileName) {
+        String response = "List document for: " + fileName;
+        return Response.ok().entity(response).build();
+    }
+
+    @DELETE
+    @Path("/{name}")
+    public void deleteDocument(@PathParam("name") String fileName) {
+        throw new UnsupportedOperationException();
+    }
+
+    @POST
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public void uploadDocument(@FormDataParam("file") InputStream inputStream,
+                               @FormDataParam("notes") FormDataBodyPart notes) {
+        throw new UnsupportedOperationException();
     }
 }
